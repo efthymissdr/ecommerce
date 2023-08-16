a6e6f683069e6ebaaacf318ed378ea53842db9eb
efthymissdr <esdiares@outlook.com>
2023-08-16 15:24:03 +0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import searchbar from "./Components/SearchBar/SearchBar";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
