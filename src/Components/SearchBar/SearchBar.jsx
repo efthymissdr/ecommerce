fd14d24ce47bf4914a133d5c4c3e859933ab4022
efthymissdr <esdiares@outlook.com>
2023-08-18 16:54:47 +0300

    inputdeleted

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -4,11 +4,7 @@ export default function SearchBar(props) {
   return (
     <div className={s.searchbar}>
       <i className="fa-solid fa-magnifying-glass" />
-      <input
-        className={s.input}
-        type="search"
-        placeholder="Search products by title"
-      />
+      <input type="search" placeholder="Search products by title" />
     </div>
   );
 }
