3da838804b59c69f64ca6373fdd725f7cd54c88e
efthymissdr <esdiares@outlook.com>
2023-08-22 13:55:41 +0300

    Created FilterList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import FilterDropdown from "./components/FilterDropdown/FilterDropdown";
 import s from "./App.module.css";
+import SearchBar from "./components/SearchBar/SearchBar";
+import FiltersList from "./FiltersList/FiltersList";
 
 export default function App() {
-  const onOptionChangeHandler = (event) => {
-    console.log("User Selected Value - ", event.target.value);
-  };
-
   return (
-    <div className={s.container}>
-      <FilterDropdown
-        options={["Apple", "Samsung", "Xiaomi", "Huawei", "LG", "OnePlus"]}
-        title="Manufacturer"
-        onSelectOption={onOptionChangeHandler}
-      />
-      <FilterDropdown
-        options={['Up to 5.4"', '5.5" - 5.9"', '6.0" - 6.4"', '6.5" and above']}
-        title="Display"
-        onSelectOption={onOptionChangeHandler}
-      />
-      <FilterDropdown
-        options={["Up to 150€", "150 - 400€", "400 - 850€", "850€ and above"]}
-        title="Price"
-        onSelectOption={onOptionChangeHandler}
-      />
-      <FilterDropdown
-        options={[
-          "At least 64GB",
-          "At least 128GB",
-          "At least 256GB",
-          "At least 512GB",
-        ]}
-        title="Storage"
-        onSelectOption={onOptionChangeHandler}
-      />
-    </div>
+    <>
+      <SearchBar />
+      <FiltersList />
+    </>
   );
 }
