2187d91f0dc6453f0731a1ace33d8aabbb8bd057
efthymissdr <esdiares@outlook.com>
2023-08-25 12:36:06 +0300

    Fixed multiple styling aspects to match the given layout of the app: -Fixed searchbar width,color and height. -Fixed FilterDropDown component style (border,box shadow etc). -Fixed ProductCard styling (border,height,width,padding). -Fixed bigcontainer styling. -Added border to bigContainer.

--- src/ProductCardGrid/ProductCardGrid.jsx
+++ src/ProductCardGrid/ProductCardGrid.jsx
@@ -35,11 +35,63 @@ const products = [
     title: "Product Name",
     image: "https://placehold.co/60x60",
   },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
+  {
+    title: "Product Name",
+    image: "https://placehold.co/60x60",
+  },
 ];
+
+function handleClick() {
+  console.log("in cardClick");
+}
 
 export default function ProductCardGrid() {
   return (
-    <div className={s.container}>
+    <div className={s.container} onClick={handleClick}>
       {products.map((product, index) => (
         <ProductCard image={product.image} title={product.title} key={index} />
       ))}
