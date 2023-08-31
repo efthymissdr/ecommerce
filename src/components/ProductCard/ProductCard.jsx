68db7b3e21611578cae88a89d69ee692f84bea95
efthymissdr <esdiares@outlook.com>
2023-08-31 14:58:07 +0300

    Fixed ProductCard styling

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import s from "./ProductCard.module.css";
 export default function ProductCard({ image, title, onClick }) {
   return (
     <div className={s.card} onClick={onClick}>
-      <img src={image} className={s.productImage} />
+      <img src={image} height={120} width={120} className={s.productImage} />
       <h2 className={s.description}>{title}</h2>
     </div>
   );
